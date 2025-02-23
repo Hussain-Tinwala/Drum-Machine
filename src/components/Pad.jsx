877ae72272f518ae9f086ab587525d4fc66e49eb
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-02-23 09:13:31 +0530

    Add logic for audio play on click of each pad

---
+++ src/components/Pad.jsx
@@ -0,0 +1,46 @@
+import { useRef, useState } from "react"
+
+export default function Pad(props) {
+    const audioref= useRef(null)
+    const [backgroundColor, setBackgroundColor]= useState()
+
+    const playAudio= ()=>{
+        setBackgroundColor('blue')
+        if(audioref.current)
+        {
+            audioref.current.play();
+            props.displayaudioText(props.audioClip)
+            console.log(props.audioClip)
+        }
+
+        setTimeout(()=>{
+            setBackgroundColor(null)
+        }, 200)
+        
+    }
+    return (
+        <button
+            className="drum-pad"
+            id={props.id}
+            onClick={playAudio}
+            style={{
+                background: backgroundColor
+            }}
+
+        >
+            {/* {console.log(props)} */}
+            {props.innerText}
+            <audio
+                ref={audioref}
+                src={`/audio/${props.audioClip}.mp3`}
+                className="clip"
+                id={props.innerText}
+                onError={(e) => console.log("Error loading audio:", e)}
+            >
+
+            </audio>
+
+        </button>
+
+    )
+}
