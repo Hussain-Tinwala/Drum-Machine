485682f7bfef615988d974724e1301a9aa67c26b
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-02-23 09:12:22 +0530

    Add data for Drum Pads

---
+++ src/components/padsData.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        id: 1,
+        innerText: 'Q',
+        audioClip: 'Heater 1'
+
+    },
+    {
+        id: 2,
+        innerText: 'W',
+        audioClip: 'Heater 2'
+
+    },
+    {
+        id: 3,
+        innerText: 'E',
+        audioClip: 'Heater 3'
+
+    },
+    {
+        id: 4,
+        innerText: 'A',
+        audioClip: 'Heater 4'
+
+    },
+    {
+        id: 5,
+        innerText: 'S',
+        audioClip: 'Clap'
+
+    },
+    {
+        id: 6,
+        innerText: 'D',
+        audioClip: 'Open-HH'
+
+    },
+    {
+        id: 7,
+        innerText: 'Z',
+        audioClip: 'Kick-n-Hat'
+
+    },
+    {
+        id: 8,
+        innerText: 'X',
+        audioClip: 'Kick'
+
+    },
+    {
+        id: 9,
+        innerText: 'C',
+        audioClip: 'Closed'
+
+    }
+]
